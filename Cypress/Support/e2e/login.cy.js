d6718c677f69581b7582ba9b4e29b932e9f8f532
Sohaib-Awan <sohaibmahmood370@gmail.com>
2022-08-05 15:52:15 +0500

    first-test

---
+++ Cypress/Support/e2e/login.cy.js
@@ -0,0 +1,57 @@
+describe('backoffice testing', () => {
+  it('login test case 1', () => {
+    cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+    cy.wait(3000)
+  cy.get("input[name=name]").type(0);
+  cy.get("input[name=password]").type(31862018).type("{enter}");
+  cy.wait(3000)  
+})
+    it('login test case 2', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+    cy.get("input[name=name]").type(1121121);
+    cy.get("input[name=password]").type(31862018).type("{enter}");
+    cy.wait(3000)  
+  })
+    it('login test case 3', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+    cy.get("input[name=name]").type(0);
+    cy.get("input[name=password]").type(454454545).type("{enter}");
+    cy.wait(3000)  
+  })
+    it('login test case 4', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+    cy.get("input[name=name]").type("                ");
+    cy.get("input[name=password]").type(31862018).type("{enter}");
+    cy.wait(9000)  
+  })
+    it('login test case 5', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+    cy.get("input[name=name]").type("aaa");
+    cy.get("input[name=password]").type("aaa").type("{enter}");
+    cy.wait(9000)
+    })
+    it('login test case 6', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+      cy.get('.signin_blk').click()
+    cy.wait(3000)
+    })
+    it('login test case 7', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+      cy.get("input[name=name]").type(0);
+      cy.get('.signin_blk').click()
+    cy.wait(3000)
+    })
+    it('login test case 8', () => {
+      cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+      cy.wait(3000)
+    cy.get("input[name=name]").type(44444);
+    cy.get("input[name=password]").type(4547878).type("{enter}");
+    cy.wait(3000)  
+  })
+  })
---
+++ Cypress/Support/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+/*describe('backoffice test', () => {
+  it('Visits the BackOffice', () => {
+    cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+    cy.wait(3000)
+  cy.get("input[name=name]").type(0);
+  cy.get("input[name=password]").type(31862018).type("{enter}");
+  })
+  it('Visits the Product module', () => {
+    cy.visit('http://103.105.211.138:2020/backoffice/account/login')
+    cy.wait(3000)
+  cy.get("input[name=name]").type(0);
+  cy.get("input[name=password]").type(31862018).type("{enter}");
+  cy.wait(6000)
+  cy.get('#delivery-20-512').click()
+ 
+  cy.wait(6000)
+  cy.get('.button.addPro.button.tooltip_cvr.ng-star-inserted').click({ force: true})        
+//cy.get("input[formcontrolname=productName]").type("testing");
+
+  })
+})*/
+

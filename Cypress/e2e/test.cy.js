d8ba4caa345746c4453106101074eb735f906e86
Sohaib-Awan <sohaibmahmood370@gmail.com>
2022-08-29 15:46:24 +0500

    Newc50

--- Cypress/e2e/test.cy.js
+++ Cypress/e2e/test.cy.js
@@ -2,7 +2,7 @@ describe('backoffice testing', () => {
     it('login test case 1', () => {
       cy.visit('http://103.105.211.138:2020/backoffice/account/login')
       cy.wait(3000)
-    cy.get("input[nae=name]").type(0);
+    cy.get("input[name=name]").type(0);
     cy.get("input[name=password]").type(31862018).type("{enter}");
     cy.wait(6000)
     })
